20aab3bd700a2cabe0dafc4326c99c8ca6f681af
beader <beader@qq.com>
2019-06-27 23:28:17 +0800

    add configurable

--- gsql_ext.py
+++ gsql_ext.py
@@ -4,18 +4,22 @@ import requests
 from IPython.core.magic import (Magics, cell_magic, line_cell_magic,
                                 line_magic, magics_class)
 
+try:
+    from traitlets.config.configurable import Configurable
+    from traitlets import Unicode
+except ImportError:
+    from IPython.config.configurable import Configurable
+
 
 @magics_class
-class GSQLMagics(Magics):
-    tg_server = None
+class GSQLMagics(Magics, Configurable):
+    server = Unicode(default_value="http://tigergraph:tigergraph@localhost:14240",
+                     config=True, help="TigerGraph Server Address")
 
     @line_magic('gsql')
-    def lmagic(self, line):
-        self.tg_server = line
-
     @cell_magic('gsql')
-    def cmagic(self, line, cell):
-        url = urljoin(self.tg_server, '/gsqlserver/interpreted_query')
+    def cmagic(self, line, cell=''):
+        url = urljoin(self.server, '/gsqlserver/interpreted_query')
         r = requests.post(url, data=cell)
         return r.json()
 
